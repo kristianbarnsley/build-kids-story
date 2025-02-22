94d23d8eb39b1ba1fd65c834e1bd3ad70c1efe6f
kristianbarnsley <kristian.barnsley@googlemail.com>
2025-02-22 00:33:55 +0000

    updated secrets handling

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+#OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 MODEL="gpt-4o"
 client = OpenAI(api_key=OPENAI_API_KEY)
 
