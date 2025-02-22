186c80c68dd7eb2cb4aab9bb67dd2d167b574867
kristianbarnsley <kristian.barnsley@googlemail.com>
2025-02-22 00:39:12 +0000

    cleared old packages

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 import streamlit as st
 from openai import OpenAI
-import os
-import json
-from dotenv import load_dotenv
+# import os
+# import json
 
-
-load_dotenv()
 #OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 MODEL="gpt-4o"
